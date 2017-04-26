ad35e991cf75bb72037a4bc3e11867af94112fb4
Anthony Pallone <pallone@unlv.nevada.edu>
2017-04-26 11:58:21 -0700

    Initial commit

---
+++ Overwatch.cpp
@@ -0,0 +1,16 @@
+// Anthony Pallone
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define PERCENT_CHANCE_COMMON 58.66
+#define PERCENT_CHANCE_RARE   31.69
+#define PERCENT_CHANCE_EPIC   7.22
+#define PERCENT_CHANCE_LEGENDARY 2.43
+
+int main(void){
+
+}
