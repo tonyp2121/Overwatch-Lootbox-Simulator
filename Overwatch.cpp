015c2bc419ac7269d3931a5d3ca8dec347ab178f
Anthony Pallone <pallone@unlv.nevada.edu>
2017-09-25 14:02:56 -0700

    Added Temp Drop Chances
    Planning on going through a couple videos after the drop change to
    confirm these percentages if I cant find a source myself

--- Overwatch.cpp
+++ Overwatch.cpp
@@ -14,7 +14,18 @@ using namespace std;
 
 #define NORMAL 0
 #define EVENT 1
-
+//
+// These are temp percent chances from the old system I keep them here in case they have not changed
+//
+#define PERCENT_CHANCE_COMMON 5820
+#define PERCENT_CHANCE_RARE   3170
+#define PERCENT_CHANCE_EPIC   755
+#define PERCENT_CHANCE_LEGENDARY 255
+#define PERCENT_CHANCE_EVENT 666
+#define PERCENT_CHANCE_TOTAL 10000
+//
+//
+//
 #define PERCENT_CHANCE_RARE_CURRENCY 411
 #define PERCENT_CHANCE_EPIC_CURRENCY 243      // I can find if its a currency if we see if its greater than common chance but less thanc ommon chance + currency chance
 #define PERCENT_CHANCE_LEGENDARY_CURRENCY 42
@@ -44,11 +55,8 @@ void PurchaseItem(bool *ItemList);
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
+	
 
-	//holding output for visual studio this code can be ignored if on linux
-	cin.clear();
-	cin.ignore();
-	cin.get();
 	return 0;
 }
 
